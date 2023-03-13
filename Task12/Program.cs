3a88e4c39fe7af6ebdff22130199220b257faf70
Eduard <raikhlined@gmail.ru>
2023-03-14 00:01:59 +0300

    done Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если число 1 не кратно числу 2, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+int Remainder(int num1, int num2)
+{
+    return num1 % num2;
+}
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int remainder = Remainder (number1, number2);
+
+if (remainder == 0) Console.WriteLine("кратно");
+else Console.WriteLine($"не кратно, остаток {remainder}");
