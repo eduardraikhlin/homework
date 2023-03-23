170f9cc48a05f6b6e9ad53f1cf38d910a70b8a75
Eduard <raikhlined@gmail.ru>
2023-03-23 13:27:12 +0300

    done Task35

---
+++ Task35/Program.cs
@@ -0,0 +1,50 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int [] array = CreateArrayRndInt(5, -100, 100);
+PrintArray(array);
+Console.WriteLine();
+int count = CountElements(array, 10, 99);
+Console.WriteLine(count);
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray (int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    } 
+    Console.Write("]");
+}
+
+int CountElements(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>=min && array[i]<=max) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
