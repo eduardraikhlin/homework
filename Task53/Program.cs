5fd2238f64cdcbe788c6b36b915061773383a2f6
Eduard <raikhlined@gmail.ru>
2023-04-01 12:11:27 +0300

    done Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,49 @@
+﻿// 53. Задайте двумерный массив. Напишите программу,
+// которая поменяет первую и последнюю строки местами.
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+ChangeRows(array2d);
+PrintMatrix(array2d);
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4} ");
+        } 
+    Console.WriteLine("|");
+    }
+}
+
+void ChangeRows(int[,] matrix)
+{
+    int firstRow = 0;
+    int LastRow = matrix.GetLength(0) - 1;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[firstRow, j];
+        matrix[firstRow, j] = matrix[LastRow, j];
+        matrix[LastRow, j] = temp;
+    }
+}
+
